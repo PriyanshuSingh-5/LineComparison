eaa84d3c2e7272d5e411eccbcc05cb0ea681e953
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-08 03:41:30 +0530

    [Priyanshu]modified length of line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace LineProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison");
+       
+
+                Console.WriteLine("enter x1 value:");
+                string input1 = Console.ReadLine();
+                int x1 = Convert.ToInt32(input1);
+
+                Console.WriteLine("enter x2 value:");
+                string input2 = Console.ReadLine();
+            int x2 = Convert.ToInt32(input2);
+
+            Console.WriteLine("enter y1 value:");
+                string input3 = Console.ReadLine();
+            int y1 = Convert.ToInt32(input3);
+
+            Console.WriteLine("enter y2 value:");
+                string input4 = Console.ReadLine();
+            int y2 = Convert.ToInt32(input4);
+
+            Length(x1, x2, y1, y2);
+
+        }
+        private static void Length( int x1,int x2, int y1,int y2)
+        {
+            double Length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(Length);
         }
     }
 }
