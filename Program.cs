b5a34c824b29895a542cf49769f1b384bdb21202
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-08 03:18:02 +0530

    [Priyanshu]added welcome to line comparison

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison");
+        }
+    }
+}
