71cb795000c044547ba5c06f9d6c1d71754fd4ee
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-08 19:55:19 +0530

    [priyanshu]equality of two lines

--- Program.cs
+++ Program.cs
@@ -6,31 +6,66 @@ namespace LineProblem
     {
         static void Main(string[] args)
         {
-       
+            Console.WriteLine(" Enter co-ordinates of First Line");
 
-                Console.WriteLine("enter x1 value:");
-                string input1 = Console.ReadLine();
-                int x1 = Convert.ToInt32(input1);
+            Console.WriteLine("Enter value of x1:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of x2:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of y1:");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of y2:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double Length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
 
-                Console.WriteLine("enter x2 value:");
-                string input2 = Console.ReadLine();
-            int x2 = Convert.ToInt32(input2);
+            Console.WriteLine("Enter co-ordinate of Second Line");
 
-            Console.WriteLine("enter y1 value:");
-                string input3 = Console.ReadLine();
-            int y1 = Convert.ToInt32(input3);
+            Console.WriteLine("Enter value of X1:");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of X2:");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of Y1:");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of Y2:");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+            double Length2 = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
 
-            Console.WriteLine("enter y2 value:");
-                string input4 = Console.ReadLine();
-            int y2 = Convert.ToInt32(input4);
+            bool status = Length1.Equals(Length2);
+            if (status)
+            {
+                Console.WriteLine("Length of Line 1 = Length of Line2");
 
-            Length(x1, x2, y1, y2);
+            }
+            else
+            {
+                Console.WriteLine("Length of Line 1 is not equal to Line 2");
+            }
 
-        }
-        private static void Length( int x1,int x2, int y1,int y2)
-        {
-            double Length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine(Length);
+            Console.WriteLine("The Length of Line1 is " + Length1);
+            Console.WriteLine("The Length of Line2 is " + Length2);
+            Console.Read();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
